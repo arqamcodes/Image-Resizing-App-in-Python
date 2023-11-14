0cac48676f579c509c3ab741444f8117abd072aa
Muhammad Arqam <68507521+arqamcodes@users.noreply.github.com>
2023-11-14 22:10:40 +0500

    Update imageresizer.py

--- imageresizer.py
+++ imageresizer.py
@@ -1,16 +1,16 @@
-# import cv2
+import cv2
 
-# src = cv2.imread("arqam.jpg", cv2.IMREAD_UNCHANGED)
+src = cv2.imread("arqam.jpg", cv2.IMREAD_UNCHANGED)
 
-# #Percent by which the image is resized
-# scale_percent = 50
+#Percent by which the image is resized
+scale_percent = 50
 
-# #Calculate the 50 percent of original dimensions
-# new_width = int(src.shape[1] * scale_percent/100)
-# new_height = int(src.shape[0] * scale_percent/100)
+#Calculate the 50 percent of original dimensions
+new_width = int(src.shape[1] * scale_percent/100)
+new_height = int(src.shape[0] * scale_percent/100)
 
-# #Resize image 
-# output = cv2.resize(src, (new_width, new_height))
+#Resize image 
+output = cv2.resize(src, (new_width, new_height))
 
-# cv2.imwrite('newimage.png', output)
-# cv2.waitKey(0)  
+cv2.imwrite('newimage.png', output)
+cv2.waitKey(0)  
